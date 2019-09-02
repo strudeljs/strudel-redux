86dadcaba8c93a5689beaed6d3f5778728c7a96f
Michal Graziowski <michal.graziowski@cognifide.com>
2019-09-02 14:48:11 +0200

    fix problem with multiple instantions

--- src/attachStore.js
+++ src/attachStore.js
@@ -4,17 +4,19 @@ function AttachStore(store) {
   return function AttachStoreToComponent(target) {
     const { subscriptionQueue = [] } = target.prototype;
 
-    let currentState = store.getState();
+    function handleStoreChangeWrapper() {
+      let currentState = store.getState();
 
-    function handleStoreChange() {
-      const previousState = currentState;
-      currentState = store.getState();
+      return function handleStoreChange() {
+        const previousState = currentState;
+        currentState = store.getState();
 
-      subscriptionQueue.forEach(({ observed, passive, method }) => {
-        if (isObservedChanged(observed(previousState), observed(currentState))) {
-          method.call(this, Object.assign({}, observed(currentState), passive(currentState)));
-        }
-      });
+        subscriptionQueue.forEach(({ observed, passive, method }) => {
+          if (isObservedChanged(observed(previousState), observed(currentState))) {
+            method.call(this, Object.assign({}, observed(currentState), passive(currentState)));
+          }
+        });
+      }
     }
 
     const {
@@ -25,7 +27,7 @@ function AttachStore(store) {
     // eslint-disable-next-line no-param-reassign
     target.prototype.init = function init() {
       originalInit.call(this);
-      this.unsubscribe = store.subscribe(handleStoreChange.bind(this));
+      this.unsubscribe = store.subscribe(handleStoreChangeWrapper().bind(this));
     };
 
     // eslint-disable-next-line no-param-reassign
